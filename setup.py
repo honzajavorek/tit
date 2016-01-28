bc6685807d3cb22e5e47e15fec2934a7de6efd2b
Honza Javorek <mail@honzajavorek.cz>
2016-01-28 16:06:25 +0100

    Fixed test setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 
-import sys
-import subprocess
-
 from setuptools import setup
-from setuptools.command.test import test
 
 
 def readme():
@@ -11,17 +7,11 @@ def readme():
         return f.read()
 
 
-class TestCommand(test):
-    def run(self):
-        test.run(self)
-        sys.exit(subprocess.call(['flake8', 'tit.py', 'setup.py']))
-
-
 setup(
     name='tit',
     description='Better titulky.com search',
     long_description=readme(),
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/honzajavorek/tit',
     author='Honza Javorek',
     author_email='mail@honzajavorek.cz',
@@ -34,16 +24,10 @@ setup(
         'lxml',
         'sh',
     ],
-    tests_require=[
-        'flake8',
-    ],
     entry_points='''
         [console_scripts]
         tit=tit:cli
     ''',
-    cmdclass={
-        'test': TestCommand,
-    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
